d451a59ab0ef01dc887b42580b047868e850c43a
Madhav Bhardwaj <69211934+Zugar-Daddy@users.noreply.github.com>
2026-07-23 22:25:45 +0530

    Things are pretty now

--- logic.js
+++ logic.js
@@ -1,3 +1,8 @@
+let size = Number(prompt('Hey Man, what size of grid do you want?'));
+size = size >= 100 ? 100 : size;
+let boxSize = Math.floor(800 / size);
+
+// container size is 800 x 800
 const container = document.querySelector(".grid-box-container");
 const rainbow = document.querySelector(".rainbow");
 const black = document.querySelector(".black");
@@ -22,33 +27,28 @@ let COLOR = 'black';
 let setColor = (col) => COLOR = col;
 
 
-createGrid(100, 100, container, 'grid-box');
+createGrid(size, size, container, 'grid-box');
 
-function createGrid(gridx, gridy, obj, boxClass){
-
-    let i = gridx;
-    while (i > 0){
-        let j = gridy;
-
+function createGrid(rows, cols, container, boxClass) {
+    for (let i = 0; i < rows; i++) {
         const col = document.createElement("div");
         col.classList.add("col");
+        container.appendChild(col);
 
-        obj.appendChild(col);
-        while(j > 0){
+        for (let j = 0; j < cols; j++) {
             const sq = document.createElement("div");
             sq.classList.add(boxClass);
+            sq.style.width = `${boxSize}px`;
+            sq.style.height = `${boxSize}px`;
 
-            sq.addEventListener('mouseover', changeColor);
-            sq.addEventListener('mousedown', changeColor);
+            sq.addEventListener("mousedown", changeColor);
+            sq.addEventListener("mouseover", changeColor);
 
             col.appendChild(sq);
-
-            j--;
         }
-        i--;
     }
-
 }
+
 
 // event object is passed
 function changeColor(e){
